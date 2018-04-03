8e33a0ccd6faaec1d3d2a24584aba7fd94cee5c4
subhamayd2 <bloodyshower@gmail.com>
2018-04-03 20:46:56 +0530

    Initial commit

---
+++ app/src/main/java/com/aztechcorps/texter/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.aztechcorps.texter.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aztechcorps.texter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
